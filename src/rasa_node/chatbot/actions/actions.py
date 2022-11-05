254a2c4d1827c5f752b531ced5d3c2b55764e4d8
S0nFra <80450457+S0nFra@users.noreply.github.com>
2022-11-05 13:50:59 +0100

    Complete actions first version
    Completed a first version for custom actions (insert, remove and show).
    
    In order to achieve this we had to modify following things...
    
    - nlu.yml
    we have merge all the insert intents in a unique intent "insert". This because there were some issues of missclassification between insert and delete intent.
    
    - domain.yml
    to update the intents and add a new actions named "action_reset_user": to reset username slot, so you can switch to another user by identification intend
    
    - stories.yml and rules.yml (omg this one)
    to implements in some way a pattern/form of conversation and activate proprerly the custom actions
    
    #TODO
    integrade "deadline" and improve stories/rules and training data to better perform

--- src/rasa_node/chatbot/actions/actions.py
+++ src/rasa_node/chatbot/actions/actions.py
@@ -17,6 +17,16 @@ import sqlite3 as sql
 
 DB_PATH=str(pathlib.Path(__file__).parent.absolute()) + "/../../database.db"
 
+CREATE_TABLE_QUERY = """CREATE TABLE todolist(
+	tag INTEGER PRIMARY KEY AUTOINCREMENT,
+  	user varchar(255) NOT NULL,
+	category varchar(255),
+	activity varchar(255),
+	deadline datetime,
+	unique(user,activity,category)
+);
+"""
+
 def get_connetion(path = DB_PATH):
     con = sql.connect(path)
     cur = con.cursor()
@@ -24,13 +34,8 @@ def get_connetion(path = DB_PATH):
     try:
         cur.execute("SELECT * FROM todolist")
     except sql.OperationalError as e:
-        print("Table creation...",end=' ')
-        cur.execute('''CREATE TABLE todolist(
-        Tag INTEGER PRIMARY KEY AUTOINCREMENT,
-        User varchar(255) NOT NULL,
-        Category varchar(255),
-        Activity varchar(255),
-        Deadline datetime);''')
+        print("Table creation...")
+        cur.execute(CREATE_TABLE_QUERY)
     
     return con, cur
 
@@ -69,12 +74,17 @@ class ActionInsert(Action):
 
         con, cur = get_connetion()
         query = f"insert into todolist(user, category, activity, deadline) values(\'{username}\',\'{category}\',\'{activity}\',0)"
-        cur.execute(query)
+        try:
+            cur.execute(query)
+        except sql.OperationalError as e:
+            dispatcher.utter_message(text = "Somethigs goes wrong! maybe empty database")
+        except sql.IntegrityError as err:
+            dispatcher.utter_message(text = "This activity already exists, try something else")
+            return [SlotSet("category", None), SlotSet("activity", None), SlotSet("deadline", None)]
         con.commit()
         con.close()
         
         print(query)
-
         dispatcher.utter_message(text = "Perfect! I have added \"" + activity + "\" in \"" + category + "\"")
         
         return [SlotSet("category", None), SlotSet("activity", None), SlotSet("deadline", None)]
@@ -101,28 +111,28 @@ class ActionRemove(Action):
         activity = tracker.get_slot("activity")
         con, cur = get_connetion()
 
+        # Elimina una categoria
         if category is not None and activity is None:
             query = f"delete from todolist where user=\'{username}\' and category = \'{category}\' "
-            cur.execute(query)
+            res = cur.execute(query)
+            tmp = res.fetchall()
+            if len(tmp) == 0:
+                dispatcher.utter_message(text = "Something wrong, maybe no category")
+                return [SlotSet("category", None)]
             con.commit()
             con.close()
             print(query)
             dispatcher.utter_message(text = "Perfect \""+ username + "\"! I have deleted the category \"" + category + "\"")
             return [SlotSet("category", None)]
 
-
-        if activity is None:
-            dispatcher.utter_message(text = "Please insert activity")
-            return []
-
-        # deadline = tracker.get_slot("deadline")
-        # if deadline is None:
-        #     dispatcher.utter_message(text = "Do you want insert a deadline?") 
-        #     return []
-        # print("deadline setted:", deadline)
-
+        # elimina una attività in una categoria
         query = f"delete from todolist where user=\'{username}\' and category = \'{category}\' and activity = \'{activity}\'"
-        cur.execute(query)
+        res = cur.execute(query)
+        tmp = res.fetchall()
+        if len(tmp) == 0:
+            dispatcher.utter_message(text = "Something wrong, maybe no activity")
+            return [SlotSet("category", None), SlotSet("activity", None), SlotSet("deadline", None)]
+
         con.commit()
         con.close()
         print(query)
@@ -148,25 +158,47 @@ class ActionShow(Action):
         # controllare che tutte le entità siano presenti
         # se manca un entità richiederne l'inserimento
         category = tracker.get_slot("category")
-        if category is None:
-            dispatcher.utter_message(text = "Please insert category")
-            return []
+        con, cur = get_connetion()
 
-        activity = tracker.get_slot("activity")
-        if activity is None:
-            dispatcher.utter_message(text = "Please insert activity")
-            return []
+        # data la categoria mostrami tutte le attività in essa contenute
+        if category is not None:
+            query = f"select tag, activity, deadline from todolist where user=\'{username}\' and category = \'{category}\'"
+            res = cur.execute(query)
+            tmp = res.fetchall()
+            if len(tmp) == 0:
+                dispatcher.utter_message(text = "Something wrong, maybe no activities")
+                return [SlotSet("category", None)] 
+            
+            dispatcher.utter_message(text = f"Ok {username}, showing activities in {category}")
+            for col in tmp:
+                dispatcher.utter_message(text = "Tag: " + str(col[0]) + "\t activity: " + str(col[1]) + "\t deadline " + str(col[2]))
+            con.commit()
+            con.close()
+            print(query)
+            return [SlotSet("category", None)]            
 
-        # deadline = tracker.get_slot("deadline")
-        # if deadline is None:
-        #     dispatcher.utter_message(text = "Do you want insert a deadline?") 
-        #     return []
-        # print("deadline setted:", deadline)
-
-        query = f"select * from todolist "
+        # mostrami tutte le categorie per l'unte corrente
+        query = f"select category from todolist where user=\'{username}\'"
+        res = cur.execute(query)
+        tmp = set(res.fetchall())
+        if len(tmp) == 0:
+            dispatcher.utter_message(text = "Something wrong, maybe no categories")
+            return [SlotSet("category", None), SlotSet("activity", None), SlotSet("deadline", None)]
         
+        dispatcher.utter_message(text = f"Ok {username}, showing your category")
+        for i,col in enumerate(tmp):
+              dispatcher.utter_message(text = str(i+1) + " " + str(col[0]))
         print(query)
-
-        dispatcher.utter_message(text = "Showing...")
         
         return [SlotSet("category", None), SlotSet("activity", None), SlotSet("deadline", None)]
+
+class ActionResetUser(Action):
+    
+    def name(self) -> Text:
+        return "action_reset_user"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        return [SlotSet("username", None)]  
