1c22b4aad91831b20f5574da41ed9b28fa237618
MarioAmato99 <m.amato72@studenti.unisa.it>
2022-12-08 17:51:17 +0100

    update of the reminder file

--- src/ros_audio_pkg/src/reminder.py
+++ src/ros_audio_pkg/src/reminder.py
@@ -3,7 +3,6 @@ import rospy
 from datetime import datetime,timedelta
 from std_msgs.msg import String
 import sqlite3 as sql
-from rasa_ros.chatbot.actions.actions import get_connetion
 
 CREATE_TABLE_QUERY = """CREATE TABLE todolist(
 	tag INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,17 +15,17 @@ CREATE_TABLE_QUERY = """CREATE TABLE todolist(
 );
 """
 
-# def get_connetion(path = "Music\\database.db"):
-#     con = sql.connect(path)
-#     cur = con.cursor()
+def get_connetion(path = "Music\\database.db"):
+    con = sql.connect(path)
+    cur = con.cursor()
 
-#     try:
-#         cur.execute("SELECT * FROM todolist")
-#     except sql.OperationalError as e:
-#         print("Table creation...")
-#         cur.execute(CREATE_TABLE_QUERY)
+    try:
+        cur.execute("SELECT * FROM todolist")
+    except sql.OperationalError as e:
+        print("Table creation...")
+        cur.execute(CREATE_TABLE_QUERY)
     
-#   return con, cur
+    return con, cur
 
 def callback(username):
     pub = rospy.Publisher('give_reminder', String, queue_size=10)
@@ -34,23 +33,16 @@ def callback(username):
     
     con, cur = get_connetion()
     str = None
-    query = f"select deadline from todolist where reminder=true and user='francesco'"
-    print(query)
+    query = f"select deadline from todolist where reminder=true and user={username}"
     res = cur.execute(query)
-    print(res)
     tmp = res.fetchall()
-    print(tmp)
     if len(tmp) == 0:
-        print("ciao")
         return None
-    print(tmp)
     for el in tmp:
-        deadline = datetime.strptime(el,'%Y-%m-%dT%H:%M:%S.%fZ')
-        print(deadline)
-        if (deadline - timedelta(hours=1) < datetime.now()):
+        deadline = datetime.fromisoformat(el)
+        if (deadline.replace(tzinfo=None) - timedelta(hours=1) < datetime.now()):
             str += el + "\n"
     con.close()
-    print(str)
     pub.publish(str)
     return str
 
